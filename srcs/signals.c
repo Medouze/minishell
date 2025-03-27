bdd819dbb7b3ce05040def69ebd44e8737f1cd5c
medouze <medylavergne@hotmail.com>
2025-03-27 12:48:20 +0100

    fixed stuff

--- srcs/signals.c
+++ srcs/signals.c
@@ -6,28 +6,29 @@
 /*   By: mlavergn <mlavergn@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 21:11:23 by mlavergn          #+#    #+#             */
-/*   Updated: 2025/03/27 12:25:12 by mlavergn         ###   ########.fr       */
+/*   Updated: 2025/03/27 12:29:47 by mlavergn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 #include <termios.h>
 
-static void	restore_terminal(struct termios *term)
+void	ft_sig_handling(int sig)
 {
-	if (tcsetattr(STDIN_FILENO, TCSANOW, term) == -1)
-	{
-		perror("tcsetattr");
-	}
-}
+	struct termios	term;
 
-static void	handle_sigint(struct termios *term)
-{
-	term->c_lflag &= ~ISIG;
-	if (tcsetattr(STDIN_FILENO, TCSANOW, term) == -1)
+	if (sig != SIGINT)
+		return;
+	if (tcgetattr(STDIN_FILENO, &term) == -1)
+	{
+		perror("tcgetattr");
+		return;
+	}
+	term.c_lflag &= ~ISIG;
+	if (tcsetattr(STDIN_FILENO, TCSANOW, &term) == -1)
 	{
 		perror("tcsetattr");
-		return ;
+		return;
 	}
 	if (isatty(STDOUT_FILENO) == 0)
 		close(STDOUT_FILENO);
@@ -35,6 +36,8 @@ static void	handle_sigint(struct termios *term)
 	rl_on_new_line();
 	rl_replace_line("", 0);
 	rl_redisplay();
+	term.c_lflag |= ISIG;
+	tcsetattr(STDIN_FILENO, TCSANOW, &term);
 }
 
 void	ft_sig_heredoc(int sig)
